70c53626189bf695fc638e3d8248dded20b6e260
Mithi <mithi.sevilla@gmail.com>
2021-03-14 13:56:35 +0800

    feat: Each ToDo item has two modes now, edit mode, and view mode

--- src/components/ToDoListItem/index.js
+++ src/components/ToDoListItem/index.js
@@ -1,14 +1,10 @@
 import { useState } from "react"
-import { List, Checkbox, Button, Popconfirm, Tooltip } from "antd"
+import { List, Checkbox, Button, Popconfirm, Tooltip, Input } from "antd"
 import { DeleteFilled, EditFilled } from "@ant-design/icons"
 
-function ToDoListItem({ item, removeItem, updateItem }) {
+function CrudButtons({ item, removeItem, setEditMode }) {
     const [visible, setVisible] = useState(false)
     const [confirmLoading, setConfirmLoading] = useState(false)
-
-    const showPopconfirm = () => {
-        setVisible(true)
-    }
 
     const handleOk = () => {
         setConfirmLoading(true)
@@ -20,50 +16,179 @@ function ToDoListItem({ item, removeItem, updateItem }) {
         removeItem(item.id)
     }
 
-    const handleCancel = () => {
-        setVisible(false)
+    return (
+        <>
+            <Tooltip title="Edit this item">
+                <Button
+                    icon={<EditFilled />}
+                    style={{ margin: "5px" }}
+                    onClick={() => setEditMode(item.id)}
+                ></Button>
+            </Tooltip>
+            <Popconfirm
+                title="Are you sure you want to delete this item?"
+                visible={visible}
+                onConfirm={handleOk}
+                okButtonProps={{ loading: confirmLoading }}
+                onCancel={() => setVisible(false)}
+            >
+                <Button
+                    danger
+                    icon={<DeleteFilled />}
+                    style={{ margin: "5px" }}
+                    onClick={() => setVisible(true)}
+                />
+            </Popconfirm>
+        </>
+    )
+}
+
+function EditMode({ item, updateItem, removeEditMode }) {
+    const [incompleteValue, setIncompleteValue] = useState(item.title)
+
+    const onSubmit = () => {
+        if (item.title === incompleteValue) {
+            return
+        }
+        const updatedItem = { ...item, title: incompleteValue }
+        updateItem(updatedItem)
     }
 
+    const disableSubmit = incompleteValue === ""
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                margin: "10px",
+                width: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <div style={{ flex: 1 }}>
+                <Input
+                    rules={[
+                        { required: true, message: "This field is required" },
+                    ]}
+                    placeholder={item.title}
+                    size="large"
+                    onChange={e => {
+                        setIncompleteValue(e.target.value)
+                    }}
+                    onPressEnter={onSubmit}
+                    value={incompleteValue}
+                />
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Tooltip
+                    title={
+                        disableSubmit
+                            ? "Please enter text before submitting"
+                            : "Update this task"
+                    }
+                >
+                    <Button
+                        type="primary"
+                        style={{
+                            margin: "10px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                        onClick={onSubmit}
+                        disabled={disableSubmit}
+                    >
+                        Save
+                    </Button>
+                </Tooltip>
+                <Tooltip
+                    title={
+                        disableSubmit
+                            ? "Please enter text before submitting"
+                            : "Cancel"
+                    }
+                >
+                    <Button
+                        style={{
+                            margin: "10px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                        onClick={removeEditMode}
+                    >
+                        Cancel
+                    </Button>
+                </Tooltip>
+            </div>
+        </div>
+    )
+}
+
+function ToDoListItem({
+    item,
+    removeItem,
+    updateItem,
+    isEditMode,
+    removeEditMode,
+    setEditMode,
+}) {
     const toggleCheckBox = () => {
         updateItem({ ...item, completed: !item.completed })
     }
 
+    const view = isEditMode ? (
+        <EditMode {...{ item, updateItem, removeEditMode }} />
+    ) : (
+        <>
+            <div
+                style={{
+                    display: "flex",
+                    padding: "20px",
+                    width: "calc(100vw - 100px)",
+                }}
+                onClick={() => setEditMode(item.id)}
+            >
+                <Checkbox checked={item.completed} onChange={toggleCheckBox}>
+                    <spa>{item.title}</spa>
+                </Checkbox>
+            </div>
+
+            <div
+                style={{
+                    display: "flex",
+                    width: "100px",
+                }}
+            >
+                <CrudButtons
+                    {...{
+                        item,
+                        toggleCheckBox,
+                        updateItem,
+                        removeItem,
+                        removeEditMode,
+                        setEditMode,
+                    }}
+                />
+            </div>
+        </>
+    )
     return (
         <List.Item
             style={{
-                padding: "20px",
                 listStyleType: "none",
                 display: "flex",
                 justifyContent: "space-between",
             }}
         >
-            <div style={{ display: "flex" }}>
-                <Checkbox checked={item.completed} onChange={toggleCheckBox}>
-                    <span style={{ padding: "10px" }}>{item.title}</span>
-                </Checkbox>
-            </div>
-            <div>
-                <Tooltip title="Edit this item">
-                    <Button
-                        icon={<EditFilled />}
-                        style={{ margin: "5px" }}
-                    ></Button>
-                </Tooltip>
-                <Popconfirm
-                    title="Are you sure you want to delete this item?"
-                    visible={visible}
-                    onConfirm={handleOk}
-                    okButtonProps={{ loading: confirmLoading }}
-                    onCancel={handleCancel}
-                >
-                    <Button
-                        danger
-                        icon={<DeleteFilled />}
-                        style={{ margin: "5px" }}
-                        onClick={showPopconfirm}
-                    />
-                </Popconfirm>
-            </div>
+            {view}
         </List.Item>
     )
 }
