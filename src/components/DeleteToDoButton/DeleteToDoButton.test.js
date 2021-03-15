727e43d26e2e8e72623f1b6ae905420949e7ff22
Mithi <mithi.sevilla@gmail.com>
2021-03-16 00:45:15 +0800

    test: Refactor and add test of removeAllNotification callback

--- src/components/DeleteToDoButton/DeleteToDoButton.test.js
+++ src/components/DeleteToDoButton/DeleteToDoButton.test.js
@@ -1,18 +1,25 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import DeleteToDoButton from "."
+import { removeAllNotifications } from "../notifications"
+jest.mock("../notifications")
 
 const createId = () => (new Date() / 1).toString()
 
-const mockFunction = jest.fn()
+const removeItem = jest.fn()
+const setTrashToolTip = jest.fn()
+const setEditMode = jest.fn()
+
+const id = createId()
 
 const openedDeleteToDoButton = (
     <DeleteToDoButton
         {...{
             isTrashToolTipOpen: true,
-            removeItem: mockFunction,
-            setTrashToolTip: mockFunction,
-            setEditMode: mockFunction,
-            id: createId(),
+            removeItem,
+            setTrashToolTip,
+            setEditMode,
+            id,
         }}
     />
 )
@@ -21,10 +28,10 @@ const closedDeleteToDoButton = (
     <DeleteToDoButton
         {...{
             isTrashToolTipOpen: false,
-            removeItem: mockFunction,
-            setTrashToolTip: mockFunction,
-            setEditMode: mockFunction,
-            id: createId(),
+            removeItem,
+            setTrashToolTip,
+            setEditMode,
+            id,
         }}
     />
 )
@@ -35,31 +42,61 @@ describe("DeleteToDoButton", () => {
 
         const deleteButton = screen.getByRole("button", { name: /delete/i })
         expect(deleteButton).toBeInTheDocument()
+
         const trashIcon = screen.getByRole("img", { name: /delete/i })
-        expect(
-            screen.getByRole("tooltip", {
-                title: "Are you sure you want to delete this item?",
-            })
-        ).toBeInTheDocument()
         expect(trashIcon).toBeInTheDocument()
+
+        const confirmToolTip = screen.getByRole("tooltip", {
+            title: /Are you sure you want to delete this item?/i,
+        })
+        expect(confirmToolTip).toBeInTheDocument()
+
         expect(deleteButton).toContainElement(trashIcon)
         expect(deleteButton).toBeDisabled()
-        expect(screen.getByText("Are you sure you want to delete this item?"))
-        const cancelText = screen.getByText(/cancel/i)
-        const okText = screen.getByText(/ok/i)
-        expect(cancelText).toBeInTheDocument()
-        expect(okText).toBeInTheDocument()
 
+        const cancelText = screen.getByText(/cancel/i)
+        expect(cancelText).toBeInTheDocument()
         expect(cancelText.parentElement).toHaveProperty("type", "button")
+
+        const okText = screen.getByText(/ok/i)
+        expect(okText).toBeInTheDocument()
         expect(okText.parentElement).toHaveProperty("type", "button")
     })
 
-    test("When closed it doesn't have the tooltip nodes", async () => {
+    test("When closed it doesn't have the tooltip nodes (and button is not disabled)", async () => {
         render(closedDeleteToDoButton)
 
         const deleteButton = screen.getByRole("button", { name: /delete/i })
-        expect(deleteButton).toBeInTheDocument()
-        expect(screen.queryByRole("tooltip")).not.toBeInTheDocument()
+        const confirmToolTip = screen.queryByRole("tooltip", {
+            title: /Are you sure you want to delete this item?/i,
+        })
+
+        expect(confirmToolTip).not.toBeInTheDocument()
         expect(deleteButton).not.toBeDisabled()
+    })
+
+    test("When delete button is clicked it should remove notifications", () => {
+        render(closedDeleteToDoButton)
+        const deleteButton = screen.getByRole("button", {
+            name: /delete item/i,
+        })
+        expect(deleteButton).toBeInTheDocument()
+
+        let confirmToolTip = screen.queryByRole("tooltip", {
+            title: /Are you sure you want to delete this item?/i,
+        })
+        expect(confirmToolTip).not.toBeInTheDocument()
+
+        userEvent.click(deleteButton)
+
+        expect(removeAllNotifications).toBeCalledTimes(1)
+    })
+
+    test("When confirm button is clicked in the tooltip, it should call the removeItem callback prop", () => {
+        render(openedDeleteToDoButton)
+        const okText = screen.getByText(/ok/i)
+        const confirmButton = okText.parentElement
+        userEvent.click(confirmButton)
+        expect(removeItem).toBeCalledWith(id)
     })
 })
