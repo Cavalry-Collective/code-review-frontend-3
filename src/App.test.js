2e3784becd2e020ac32a21e22871f0823ac62270
Mithi <mithi.sevilla@gmail.com>
2021-03-15 22:18:04 +0800

    test/refactor: Refactor app test for readability

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,9 @@ import { render, screen } from "@testing-library/react"
 import App from "./App"
 import { useMockFetchToDo } from "./hooks"
 
-jest.mock("./hooks")
-
 // https://stackoverflow.com/questions/64813447/cannot-read-property-addlistener-of-undefined-react-testing-library
 // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+
 global.matchMedia =
     global.matchMedia ||
     function () {
@@ -14,19 +13,19 @@ global.matchMedia =
             removeListener: jest.fn(),
         }
     }
-/*
-ALTERNATIVELY:
 
-delete window.matchMedia
-    window.matchMedia = (query) => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
+// ALTERNATIVELY
+
+/*delete window.matchMedia
+window.matchMedia = query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
 })
 */
 
@@ -55,6 +54,8 @@ const USE_FETCH_TODO_RETURN_VALUE = [
         errorMessage: null,
     },
 ]
+
+jest.mock("./hooks")
 
 test("Renders To Do Title", async () => {
     useMockFetchToDo.mockReturnValue(USE_FETCH_TODO_RETURN_VALUE)
