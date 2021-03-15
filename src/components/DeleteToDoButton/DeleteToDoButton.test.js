b8f493264f5873fcc4f1d0d6b6528938b7b415eb
Mithi <mithi.sevilla@gmail.com>
2021-03-15 17:34:38 +0800

    test: 2 test for checking contents of DeleteToDo button

--- src/components/DeleteToDoButton/DeleteToDoButton.test.js
+++ src/components/DeleteToDoButton/DeleteToDoButton.test.js
@@ -1,27 +1,61 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import DeleteToDoButton from "."
 
 const createId = () => (new Date() / 1).toString()
 
 const mockFunction = jest.fn()
 
-test("DeleteToDoButton: contains the delete button and has trash icon as its child ", async () => {
-    const id = createId()
-    render(
-        <DeleteToDoButton
-            {...{
-                isTrashToolTipOpen: false,
-                removeItem: mockFunction,
-                setTrashToolTip: mockFunction,
-                setEditMode: mockFunction,
-                id,
-            }}
-        />
-    )
+const openedDeleteToDoButton = (
+    <DeleteToDoButton
+        {...{
+            isTrashToolTipOpen: true,
+            removeItem: mockFunction,
+            setTrashToolTip: mockFunction,
+            setEditMode: mockFunction,
+            id: createId(),
+        }}
+    />
+)
 
-    const deleteButton = screen.getByRole("button", { Name: /delete/i })
+test("DeleteToDoButton: Expect an opened DeleteToDoButton to have the necessary nodes and their state ", async () => {
+    render(openedDeleteToDoButton)
+
+    const deleteButton = screen.getByRole("button", { name: /delete/i })
     expect(deleteButton).toBeInTheDocument()
-    const trashIcon = screen.getByRole("img", { "aria-label": /delete/i })
+    const trashIcon = screen.getByRole("img", { name: /delete/i })
+    expect(screen.getByRole("tooltip")).toBeInTheDocument()
     expect(trashIcon).toBeInTheDocument()
-    expect(trashIcon.parentElement).toBe(deleteButton)
+    expect(deleteButton).toContainElement(trashIcon)
+    expect(deleteButton).toBeDisabled()
+    expect(screen.getByText("Are you sure you want to delete this item?"))
+    const cancelText = screen.getByText(/cancel/i)
+    const okText = screen.getByText(/ok/i)
+    expect(cancelText).toBeInTheDocument()
+    expect(okText).toBeInTheDocument()
+
+    expect(cancelText.parentElement).toHaveProperty("type", "button")
+    expect(okText.parentElement).toHaveProperty("type", "button")
+})
+
+const closedDeleteToDoButton = (
+    <DeleteToDoButton
+        {...{
+            isTrashToolTipOpen: false,
+            removeItem: mockFunction,
+            setTrashToolTip: mockFunction,
+            setEditMode: mockFunction,
+            id: createId(),
+        }}
+    />
+)
+test("DeleteToDoButton: Expect anclosed DeleteToDoButton to have the necessary nodes and their state ", async () => {
+    render(closedDeleteToDoButton)
+
+    const deleteButton = screen.getByRole("button", { name: /delete/i })
+    expect(deleteButton).toBeInTheDocument()
+    const trashIcon = screen.getByRole("img", { name: /delete/i })
+    expect(screen.queryByRole("tooltip")).not.toBeInTheDocument()
+    expect(trashIcon).toBeInTheDocument()
+    expect(deleteButton).not.toBeDisabled()
 })
