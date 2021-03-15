28e30f3855d7909cd3ebda404f7b9655155c2f43
Mithi <mithi.sevilla@gmail.com>
2021-03-16 00:53:51 +0800

    test: Add more tests for NewToDoForm

--- src/components/NewToDoForm/NewToDoForm.test.js
+++ src/components/NewToDoForm/NewToDoForm.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react"
 import NewToDoForm from "."
 import userEvent from "@testing-library/user-event"
+import { removeAllNotifications } from "../notifications"
+jest.mock("../notifications")
 
 const newTitle = "doesn't matter"
 const addItem = jest.fn()
@@ -14,22 +16,35 @@ const newToDoForm = (
 describe("NewToDoForm", () => {
     test("Clicking the input field will call the necessary callback functions given as prop", () => {
         render(newToDoForm)
+        const addButton = screen.getByRole("button", { name: /add/i })
+        expect(addButton).toBeInTheDocument()
+        expect(addButton).toBeDisabled()
+
         const inputField = screen.getByRole("textbox", { value: "" })
 
         expect(inputField).toBeInTheDocument()
         userEvent.click(inputField)
 
         expect(setEditMode).toHaveBeenCalledTimes(1)
+        expect(setEditMode).toHaveBeenCalledWith(null)
+
         expect(setTrashToolTip).toHaveBeenCalledTimes(1)
+        expect(setTrashToolTip).toHaveBeenCalledWith(null)
+
+        expect(removeAllNotifications).toHaveBeenCalledTimes(1)
         expect(addItem).not.toHaveBeenCalled()
     })
 
     test("Typing and clicking the button will call the necessary callback functions given as prop", () => {
         render(newToDoForm)
-        const inputField = screen.getByRole("textbox", { value: "" })
 
+        const inputField = screen.getByRole("textbox", { value: "" })
         userEvent.type(inputField, newTitle)
-        userEvent.click(screen.getByRole("button", { name: /add/i }))
+
+        let addButton = screen.getByRole("button", { name: /add/i })
+        expect(addButton).not.toBeDisabled()
+        userEvent.click(addButton)
+
         expect(addItem).toHaveBeenCalled()
     })
 })
